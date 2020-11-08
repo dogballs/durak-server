52e4dc2ba82067be7beecdf05b2fe97f2f47ed9c
Michael Radionov <micrddrgn@gmail.com>
2020-11-08 22:08:00 +0300

    Replace tape with ava

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,4 +1,4 @@
-import * as test from 'tape';
+import test from 'ava';
 
 import { ExplicitStockFactory } from './stock';
 import { CardSuite, CardRank } from './Card';
@@ -31,35 +31,33 @@ test('basic defence', (t) => {
   game.init(players);
 
   let gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 3);
-  t.equal(gameDto.currentId, 3);
-  t.equal(gameDto.defenderId, 7);
+  t.is(gameDto.attackerId, 3);
+  t.is(gameDto.currentId, 3);
+  t.is(gameDto.defenderId, 7);
 
-  t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R6)));
-  t.equal(game.toObject().currentId, 7);
-  t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R8)));
-  t.equal(game.toObject().currentId, 3);
+  t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R6)));
+  t.is(game.toObject().currentId, 7);
+  t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R8)));
+  t.is(game.toObject().currentId, 3);
   //
-  t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
-  t.equal(game.toObject().currentId, 7);
-  t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R8)));
-  t.equal(game.toObject().currentId, 3);
+  t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+  t.is(game.toObject().currentId, 7);
+  t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R8)));
+  t.is(game.toObject().currentId, 3);
   //
-  t.ok(game.act(CardFactory.make(CardSuite.Diamonds, CardRank.R8)));
-  t.equal(game.toObject().currentId, 7);
-  t.ok(game.act(CardFactory.make(CardSuite.Clubs, CardRank.R7)));
-  t.equal(game.toObject().currentId, 3);
+  t.true(game.act(CardFactory.make(CardSuite.Diamonds, CardRank.R8)));
+  t.is(game.toObject().currentId, 7);
+  t.true(game.act(CardFactory.make(CardSuite.Clubs, CardRank.R7)));
+  t.is(game.toObject().currentId, 3);
   //
-  t.ok(game.pass());
+  t.true(game.pass());
 
   gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 7);
-  t.equal(gameDto.currentId, 7);
-  t.equal(gameDto.defenderId, 3);
-  t.equal(gameDto.discardCount, 6);
-  t.equal(gameDto.stockCount, 0);
-
-  t.end();
+  t.is(gameDto.attackerId, 7);
+  t.is(gameDto.currentId, 7);
+  t.is(gameDto.defenderId, 3);
+  t.is(gameDto.discardCount, 6);
+  t.is(gameDto.stockCount, 0);
 });
 
 test('player is attacking loser #1', (t) => {
@@ -73,10 +71,9 @@ test('player is attacking loser #1', (t) => {
   game.init(players, 7);
 
   const gameDto = game.toObject();
-  t.equal(gameDto.defenderId, 7);
-  t.equal(gameDto.attackerId, 3);
-  t.equal(gameDto.currentId, 3);
-  t.end();
+  t.is(gameDto.defenderId, 7);
+  t.is(gameDto.attackerId, 3);
+  t.is(gameDto.currentId, 3);
 });
 
 test('player is attacking loser #2', (t) => {
@@ -90,10 +87,9 @@ test('player is attacking loser #2', (t) => {
   game.init(players, 3);
 
   const gameDto = game.toObject();
-  t.equal(gameDto.defenderId, 3);
-  t.equal(gameDto.attackerId, 5);
-  t.equal(gameDto.currentId, 5);
-  t.end();
+  t.is(gameDto.defenderId, 3);
+  t.is(gameDto.attackerId, 5);
+  t.is(gameDto.currentId, 5);
 });
 
 test('first attacker is selected by lowest trump', (t) => {
@@ -116,10 +112,9 @@ test('first attacker is selected by lowest trump', (t) => {
   game.init(players);
 
   const gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 7);
-  t.equal(gameDto.currentId, 7);
-  t.equal(gameDto.defenderId, 5);
-  t.end();
+  t.is(gameDto.attackerId, 7);
+  t.is(gameDto.currentId, 7);
+  t.is(gameDto.defenderId, 5);
 });
 
 test('first round attack max 5 cards', (t) => {
@@ -145,72 +140,70 @@ test('first round attack max 5 cards', (t) => {
   game.init(players);
 
   let gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 3);
-  t.equal(gameDto.currentId, 3);
-  t.equal(gameDto.defenderId, 7);
-  t.equal(gameDto.stockCount, 11);
+  t.is(gameDto.attackerId, 3);
+  t.is(gameDto.currentId, 3);
+  t.is(gameDto.defenderId, 7);
+  t.is(gameDto.stockCount, 11);
 
   // p1 attacks
   // p2 takes, max 5 cards
 
-  t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
-  t.ok(game.take());
+  t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+  t.true(game.take());
   for (let i = 0; i < 4; i++) {
-    t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+    t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
   }
-  t.notOk(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
-  t.ok(game.pass());
+  t.false(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+  t.true(game.pass());
 
   gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 3);
-  t.equal(gameDto.currentId, 3);
-  t.equal(gameDto.defenderId, 7);
-  t.equal(gameDto.discardCount, 0);
-  t.equal(gameDto.stockCount, 6);
-  t.equal(gameDto.state, GameState.Attack);
+  t.is(gameDto.attackerId, 3);
+  t.is(gameDto.currentId, 3);
+  t.is(gameDto.defenderId, 7);
+  t.is(gameDto.discardCount, 0);
+  t.is(gameDto.stockCount, 6);
+  t.is(gameDto.state, GameState.Attack);
 
   // p1 attacks
   // p2 defends, max 5 cards
 
   for (let i = 0; i < 5; i++) {
-    t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
-    t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R7)));
+    t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+    t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R7)));
   }
-  t.notOk(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+  t.false(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.DefenceShowcase);
+  t.is(gameDto.state, GameState.DefenceShowcase);
 
-  t.ok(game.pass());
+  t.true(game.pass());
 
   gameDto = game.toObject();
 
-  t.equal(gameDto.attackerId, 7);
-  t.equal(gameDto.currentId, 7);
-  t.equal(gameDto.defenderId, 3);
-  t.equal(gameDto.discardCount, 10);
-  t.equal(gameDto.state, GameState.Attack);
-  t.equal(gameDto.stockCount, 1);
+  t.is(gameDto.attackerId, 7);
+  t.is(gameDto.currentId, 7);
+  t.is(gameDto.defenderId, 3);
+  t.is(gameDto.discardCount, 10);
+  t.is(gameDto.state, GameState.Attack);
+  t.is(gameDto.stockCount, 1);
 
   // p2 attacks, max cards 6
 
   for (let i = 0; i < 5; i++) {
-    t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
-    t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R7)));
+    t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R6)));
+    t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R7)));
   }
-  t.ok(game.act(CardFactory.make(CardSuite.Spades, CardRank.R7)));
-  t.ok(game.take());
-  t.ok(game.pass());
+  t.true(game.act(CardFactory.make(CardSuite.Spades, CardRank.R7)));
+  t.true(game.take());
+  t.true(game.pass());
 
   gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 7);
-  t.equal(gameDto.currentId, 7);
-  t.equal(gameDto.defenderId, 3);
-  t.equal(gameDto.discardCount, 10);
-  t.equal(gameDto.state, GameState.Attack);
-  t.equal(gameDto.stockCount, 0);
-
-  t.end();
+  t.is(gameDto.attackerId, 7);
+  t.is(gameDto.currentId, 7);
+  t.is(gameDto.defenderId, 3);
+  t.is(gameDto.discardCount, 10);
+  t.is(gameDto.state, GameState.Attack);
+  t.is(gameDto.stockCount, 0);
 });
 
 test('second player leaving', (t) => {
@@ -239,60 +232,60 @@ test('second player leaving', (t) => {
   game.init(players);
 
   let gameDto = game.toObject();
-  t.equal(gameDto.attackerId, 7);
-  t.equal(gameDto.currentId, 7);
-  t.equal(gameDto.defenderId, 3);
+  t.is(gameDto.attackerId, 7);
+  t.is(gameDto.currentId, 7);
+  t.is(gameDto.defenderId, 3);
 
   // p3 attacking p1 with 5 cards (first discard is max 5)
   for (let i = 0; i < 5; i++) {
-    t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R6)));
-    t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R7)));
+    t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R6)));
+    t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R7)));
   }
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.DefenceShowcase);
-  t.equal(gameDto.passerId, 7);
-  t.equal(gameDto.currentId, 7);
+  t.is(gameDto.state, GameState.DefenceShowcase);
+  t.is(gameDto.passerId, 7);
+  t.is(gameDto.currentId, 7);
 
-  t.ok(game.pass());
-
-  gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.DefenceShowcase);
-  t.equal(gameDto.passerId, 5);
-  t.equal(gameDto.currentId, 5);
-
-  t.ok(game.pass());
+  t.true(game.pass());
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.Attack);
-  t.equal(gameDto.attackerId, 3);
-  t.equal(gameDto.currentId, 3);
-  t.equal(gameDto.defenderId, 5);
-  t.equal(gameDto.discardCount, 10);
-  t.equal(gameDto.stockCount, 1);
+  t.is(gameDto.state, GameState.DefenceShowcase);
+  t.is(gameDto.passerId, 5);
+  t.is(gameDto.currentId, 5);
+
+  t.true(game.pass());
+
+  gameDto = game.toObject();
+  t.is(gameDto.state, GameState.Attack);
+  t.is(gameDto.attackerId, 3);
+  t.is(gameDto.currentId, 3);
+  t.is(gameDto.defenderId, 5);
+  t.is(gameDto.discardCount, 10);
+  t.is(gameDto.stockCount, 1);
 
   // p1 has one card left after defending and 5 same cards from stock
   // p2 has all his original cards
   // p3 has one card left from attacking and 5 same cards from stock
   // p1 attacking p2 with all his 6 cards
   for (let i = 0; i < 6; i++) {
-    t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R7)));
-    t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R8)));
+    t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R7)));
+    t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R8)));
   }
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.DefenceShowcase);
-  t.equal(gameDto.passerId, 3);
-  t.equal(gameDto.currentId, 3);
+  t.is(gameDto.state, GameState.DefenceShowcase);
+  t.is(gameDto.passerId, 3);
+  t.is(gameDto.currentId, 3);
 
-  t.ok(game.pass());
+  t.true(game.pass());
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.DefenceShowcase);
-  t.equal(gameDto.passerId, 7);
-  t.equal(gameDto.currentId, 7);
+  t.is(gameDto.state, GameState.DefenceShowcase);
+  t.is(gameDto.passerId, 7);
+  t.is(gameDto.currentId, 7);
 
-  t.ok(game.pass());
+  t.true(game.pass());
 
   // p1 has one card picked up from stock (ace)
   // p2 leaves the game (no cards left)
@@ -300,28 +293,26 @@ test('second player leaving', (t) => {
   // p3 takes the turn to attack p1 instead
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.Attack);
-  t.equal(gameDto.attackerId, 7);
-  t.equal(gameDto.currentId, 7);
-  t.equal(gameDto.defenderId, 3);
-  t.equal(gameDto.discardCount, 22);
-  t.equal(gameDto.stockCount, 0);
+  t.is(gameDto.state, GameState.Attack);
+  t.is(gameDto.attackerId, 7);
+  t.is(gameDto.currentId, 7);
+  t.is(gameDto.defenderId, 3);
+  t.is(gameDto.discardCount, 22);
+  t.is(gameDto.stockCount, 0);
 
-  t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R6)));
-  t.ok(game.act(CardFactory.make(CardSuite.Hearts, CardRank.Ace)));
-
-  gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.DefenceShowcase);
-  t.equal(gameDto.passerId, 7);
-  t.equal(gameDto.currentId, 7);
-
-  t.ok(game.pass());
+  t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.R6)));
+  t.true(game.act(CardFactory.make(CardSuite.Hearts, CardRank.Ace)));
 
   gameDto = game.toObject();
-  t.equal(gameDto.state, GameState.Ended);
-  t.equal(gameDto.discardCount, 24);
-  t.equal(gameDto.stockCount, 0);
-  t.equal(players[2].toObject().lossCount, 1);
+  t.is(gameDto.state, GameState.DefenceShowcase);
+  t.is(gameDto.passerId, 7);
+  t.is(gameDto.currentId, 7);
 
-  t.end();
+  t.true(game.pass());
+
+  gameDto = game.toObject();
+  t.is(gameDto.state, GameState.Ended);
+  t.is(gameDto.discardCount, 24);
+  t.is(gameDto.stockCount, 0);
+  t.is(players[2].toObject().lossCount, 1);
 });
